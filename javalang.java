5864dfacc483093876d7bed9b14a237258cc5a47
Aishwarya <154884003+Ashwariya89@users.noreply.github.com>
2023-12-28 14:57:01 +0530

    Add files via upload

---
+++ javalang.java
@@ -0,0 +1,19 @@
+package Chapter2;
+
+public class javalang {
+	 public static void main(String[] args) {
+
+	        int intVar = 1;
+	        float floatVar = 2.3f;
+	        char charVar = 'A';
+	        double doubleVar = 2.731458;
+	        boolean boolVar = true;
+
+	        System.out.println("Integer: " + intVar);
+	        System.out.println("Float: " + floatVar);
+	        System.out.println("Character: " + charVar);
+	        System.out.println("Double: " + doubleVar);
+	        System.out.println("Boolean: " + boolVar);
+	    }
+
+}
